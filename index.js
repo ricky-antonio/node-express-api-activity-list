20998aa1bbec84ed768197cc654cd1116e7724a9
Ricardo Monterrosa <rickyantonio117@gmail.com>
2022-01-20 13:19:54 -0800

    add styles

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res) {
